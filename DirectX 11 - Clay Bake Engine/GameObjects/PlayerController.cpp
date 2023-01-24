b8338e8268730079b51bcaf291e18cc9622af361
Zoe Ridley <zoeridley@live.com>
2023-01-24 16:59:37 +0000

    Adding variables from Vertical Prototype

--- DirectX 11 - Clay Bake Engine/GameObjects/PlayerController.cpp
+++ DirectX 11 - Clay Bake Engine/GameObjects/PlayerController.cpp
@@ -1,5 +1,7 @@
 #include "PlayerController.h"
 
+//TODO: Treat this just like the old Unity Input System
+
 void PlayerController::Start()
 {
 	
@@ -7,12 +9,43 @@ void PlayerController::Start()
 
 void PlayerController::Update(float deltaTime)
 {
+	if (_jumpReset == false)
+	{
+		_jumpTimer += deltaTime;
+		
+		if (_jumpTimer > _midAirControlDelay)
+		{
+			_movementEnabled = true;
+		}
+		else
+		{
+			_movementEnabled = false;
+		}
+	}
 
+	//POLL BUTTON DOWN
 }
 
 void PlayerController::FixedUpdate(float timeStep)
 {
+	if (_movementEnabled)
+	{
+		//accelerates in direction of pressed input
+		//rb.AddForce(m_currentMove * moveSpeed);
+		//if (rb.velocity.magnitude > m_topSpeed) //clamps movement to topspeed
+		//{
+		//	rb.AddForce(-m_currentMove * moveSpeed);
+		//}
+	}
 
+	if (_isJumping)
+	{
+		if (!isFlipped)
+		{
+			//float _currentVelocity = rb.velocity.y;
+			//
+		}
+	}
 }
 
 void PlayerController::Stop()
--- DirectX 11 - Clay Bake Engine/GameObjects/PlayerController.h
+++ DirectX 11 - Clay Bake Engine/GameObjects/PlayerController.h
@@ -5,13 +5,35 @@
 class PlayerController : public Component
 {
 public:
+
+    //Variables
+    bool isFlipped;
+
+    //Functions
     void Start() override;
     void Update(float deltaTime) override;
     void FixedUpdate(float timeStep) override;
     void Stop() override;
 
 private:
+
+    //Variables
     PlayerInput* pi;
+
+    float _moveSpeed;
+    float _topSpeed;
+    float _midAirControlDelay;
+    float _jumpTimer;
+
+    bool _jumpReset;
+    bool _isJumping;
+    bool _movementEnabled = true;
+
+    //Vector2 _currentMovementVector;
+    //Vector2 _jumpForce;
+    //BoxCollider2D _groundDetector (change to raycast);
+    //FacingDirection _facing;
+    //Rigidbody2D _rb;
 
 };
 
