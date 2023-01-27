ebacb770900fcbfed3b5c8ca91c8ed24eb7985b3
Tony Wilson <57398501+TonyWilson-dev@users.noreply.github.com>
2023-01-27 12:23:28 +0000

    lever variables and start function

--- DirectX 11 - Clay Bake Engine/GameObjects/Components/LeverComponent.cpp
+++ DirectX 11 - Clay Bake Engine/GameObjects/Components/LeverComponent.cpp
@@ -1,6 +1,11 @@
 #pragma once
 #include "LeverComponent.h"
 
+void LeverComponent::start()
+{
+	m_State = false; //set state to off on start
+}
+
 void LeverComponent::Interact()
 {
 	//lever funcitonality implemented here
--- DirectX 11 - Clay Bake Engine/GameObjects/Components/LeverComponent.h
+++ DirectX 11 - Clay Bake Engine/GameObjects/Components/LeverComponent.h
@@ -3,6 +3,8 @@
 class LeverComponent : public Interactable
 {
 public:
+	bool m_State;
+	void start();
 	void Interact();
 };
   
