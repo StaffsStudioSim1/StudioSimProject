fa3b3daefded6031992b98107afbd3299f437337
William Miller <71334596+WilliamIMiller@users.noreply.github.com>
2023-01-27 14:26:18 +0000

    magnatisum object start

--- DirectX 11 - Clay Bake Engine/GameObjects/MagnetismObject.h
+++ DirectX 11 - Clay Bake Engine/GameObjects/MagnetismObject.h
@@ -1,8 +1,17 @@
 #pragma once
 #include "Component.h"
+#include"../Physics/Vectors.h"
+
+enum MagnetDirection
+{
+    Towards,
+    Away
+};
+
 class MagnetismObject :
     public Component
 {
-
+public:
+    //void isBeingMagnetised(Vector2 posOfEffector,MagnetDirection direction, )
 };
 
