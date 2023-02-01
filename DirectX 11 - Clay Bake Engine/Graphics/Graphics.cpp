8e05d5236e9c85e1a982684c1d41ce61c56f1c7a
BenjaminShawDev <57227644+BenjaminShawDev@users.noreply.github.com>
2023-02-01 17:53:38 +0000

    Updated ImGui
    Made it so an object's name can be changed (a bit buggy) and that scaling in both x and y can be linked. Updated some values and tidied it up a bit

--- DirectX 11 - Clay Bake Engine/Graphics/Graphics.cpp
+++ DirectX 11 - Clay Bake Engine/Graphics/Graphics.cpp
@@ -370,64 +370,60 @@ void Graphics::RenderFrame(Scene* scene)
 	ImGui::NewFrame();
 	//UI WINDOWS
 
+	static bool linkScaling = true;
 	ImGui::Begin("Inspector");
-	if (ImGui::TreeNode("Basic trees"))
+	if (ImGui::TreeNode("Game Objects"))
 	{
 		int loopNum = 0;
 		for (GameObject* object : ObjectHandler::GetInstance().GetAllObjects())
 		{
-
-
 			// Use SetNextItemOpen() so set the default state of a node to be open. We could
 			// also use TreeNodeEx() with the ImGuiTreeNodeFlags_DefaultOpen flag to achieve the same thing!
 			if (loopNum == 0)
 				ImGui::SetNextItemOpen(false, ImGuiCond_Once);
 
-			if (ImGui::TreeNode((void*)(intptr_t)loopNum, "GameObject %d", loopNum))
+			//if (ImGui::TreeNode((void*)(intptr_t)loopNum, "GameObject %d", loopNum))
+			if (ImGui::TreeNode(object->GetName().c_str()))
 			{
+				std::string name = object->GetName();
 				float position[2] = { object->GetTransform()->GetPosition().x, object->GetTransform()->GetPosition().y };
 				float depth = { object->GetTransform()->GetDepthPos() };
 				float rotation = { object->GetTransform()->GetRotation() };
 				float scale[2] = { object->GetTransform()->GetScale().x, object->GetTransform()->GetScale().y };
 
-				ImGui::DragFloat2("Translation XY", position, 1.0, -1000, 1000);
+				ImGui::PushItemWidth(250); // Sets the pixel width of the input boxes
+
+				if (ImGui::InputText("Name", (char*)name.c_str(), 30, ImGuiInputTextFlags_EnterReturnsTrue))
+					object->SetName(name);
+				ImGui::DragFloat("X Position", &position[0], 18.0f, -315.0f, 315.0f);
+				ImGui::DragFloat("Y Position", &position[1], 18.0f, -171.0f, 171.0f);
 				ImGui::DragFloat("Depth", &depth, 0.005f, 0.0f, 1.0f);
 				ImGui::DragFloat("Rotation", &rotation, 0.025f, 0.0f, DirectX::XM_2PI);
-				ImGui::DragFloat2("Scale", scale, 0.1, -100, 100);
+				ImGui::DragFloat2("Scale", scale, 0.05f, -100, 100);
 				ImGui::SameLine();
-				if (ImGui::Button("Scale Up"))
-				{
-					scale[0] += 1.0f;
-					scale[1] += 1.0f;
-				}
-				ImGui::SameLine();
-				if (ImGui::Button("Scale Down"))
-				{
-					scale[0] -= 1.0f;
-					scale[1] -= 1.0f;
-				}
+				ImGui::Checkbox("Link scaling", &linkScaling);
 				if (ImGui::Button("Reset"))
 				{
-					position[0] = 0.0f;
-					position[1] = 0.0f;
-					depth = 1.0f;
-					scale[0] = 2.5f;
-					scale[1] = 2.5f;
+					depth = 0.0f;
+					scale[0] = 1.0f;
+					scale[1] = 1.0f;
 					rotation = 0.0f;
-				}
-				if (ImGui::Button("Save"))
-				{
 				}
 				ImGui::TreePop();
 
 				object->GetTransform()->SetPosition(position[0], position[1]);
 				object->GetTransform()->SetDepthPos(depth);
+				if (linkScaling)
+					scale[1] = scale[0];
 				object->GetTransform()->SetScale(scale[0], scale[1]);
 				object->GetTransform()->SetRotation(rotation);
 			}
 			loopNum++;
 		}
 		ImGui::TreePop();
+	}
+	if (ImGui::Button("Save"))
+	{
 	}
 	ImGui::End();
 
