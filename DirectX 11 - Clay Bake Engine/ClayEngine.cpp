aad337ab6c8f8f19ef24b09636834902ebc9cebd
Ethan HOLLAS <h013341j@student.staffs.ac.uk>
2023-02-09 21:17:00 +0000

    Added Blank File

--- DirectX 11 - Clay Bake Engine/ClayEngine.cpp
+++ DirectX 11 - Clay Bake Engine/ClayEngine.cpp
@@ -34,7 +34,7 @@ bool ClayEngine::Initialize(HINSTANCE hInstance, std::string window_title, std::
 
 	_initialised = true;
 #if EDIT_MODE
-	SceneManager::GetInstance().LoadScene("Resources/demo.json");
+	SceneManager::GetInstance().LoadScene("Resources/blank.json");
 #else
 	SceneManager::GetInstance().LoadScene("Resources/MainMenu.json");
 #endif
