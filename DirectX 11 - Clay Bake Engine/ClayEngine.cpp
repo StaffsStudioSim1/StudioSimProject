41878d2e5d1d57393ceca4ac656ac8334dbee2e2
William Miller <71334596+WilliamIMiller@users.noreply.github.com>
2023-02-09 20:55:59 +0000

    changes

--- DirectX 11 - Clay Bake Engine/ClayEngine.cpp
+++ DirectX 11 - Clay Bake Engine/ClayEngine.cpp
@@ -34,9 +34,9 @@ bool ClayEngine::Initialize(HINSTANCE hInstance, std::string window_title, std::
 
 	_initialised = true;
 #if EDIT_MODE
-	SceneManager::GetInstance().LoadScene("Resources/Will.json");
+	SceneManager::GetInstance().LoadScene("Resources/demo.json");
 #else
-	SceneManager::GetInstance().LoadScene("Resources/Will.json");
+	SceneManager::GetInstance().LoadScene("Resources/mainmenu.json");
 #endif
 	return true;
 }
