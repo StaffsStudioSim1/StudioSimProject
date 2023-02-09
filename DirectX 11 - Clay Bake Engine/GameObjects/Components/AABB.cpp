96de7fd579b09bd07da7e571abc8cc8e1ad6812f
KitSymes <kitsymes@btinternet.com>
2023-02-09 00:30:53 +0000

    Optimised Stage Collisions + AABB Debug

--- DirectX 11 - Clay Bake Engine/GameObjects/Components/AABB.cpp
+++ DirectX 11 - Clay Bake Engine/GameObjects/Components/AABB.cpp
@@ -6,6 +6,11 @@ AABB::AABB(float width, float height)
 {
 	_width = width;
 	_height = height;
+
+#if DEBUG
+	_debugGeo = ObjectHandler::GetInstance().GetSquareGeometry();
+	_debugTex = ObjectHandler::GetInstance().LoadDDSTextureFile("Resources/Sprites/StageCollision.dds");
+#endif
 }
 
 json AABB::Write()
@@ -19,6 +24,9 @@ json AABB::Write()
 
 bool AABB::Overlaps(AABB* collider, float deltaTime)
 {
+	if (!_active || !collider->_active)
+		return false;
+
 	DirectX::BoundingBox me;
 	me.Center.x = _gameObject->GetTransform()->GetPosition().x;
 	me.Center.y = _gameObject->GetTransform()->GetPosition().y;
@@ -41,4 +49,36 @@ bool AABB::Overlaps(AABB* collider, float deltaTime)
 Vector2 AABB::GetSize()
 {
 	return Vector2(_width, _height);
+}
+
+void AABB::Render(Microsoft::WRL::ComPtr<ID3D11DeviceContext> context, ConstantBuffer& constantBuffer, Microsoft::WRL::ComPtr <ID3D11Buffer> globalBuffer)
+{
+#if DEBUG
+	if (!_active)
+		return;
+
+	DirectX::XMMATRIX world =
+		DirectX::XMMatrixScaling(_gameObject->GetTransform()->GetScale().x * _width / 2, _gameObject->GetTransform()->GetScale().y * _height / 2, 1.0f)
+		* DirectX::XMMatrixTranslation(_gameObject->GetTransform()->GetPosition().x, _gameObject->GetTransform()->GetPosition().y, _gameObject->GetTransform()->GetDepthPos());
+
+	if (_gameObject->GetTransform()->HasTransformParent())
+		world = world * _gameObject->GetTransform()->GetTransformParent()->GetWorldMatrix();
+
+	constantBuffer.mWorld = DirectX::XMMatrixTranspose(world);
+	constantBuffer.mTexCoord = {
+		1.0f, 0.0f, 0.0f, _width,
+		0.0f, 1.0f, 0.0f, _height,
+		0.0f, 0.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 0.0f
+	};
+	constantBuffer.mAlphaMultiplier = 1.0f;
+
+	context->UpdateSubresource(globalBuffer.Get(), 0, nullptr, &constantBuffer, 0, 0);
+
+	// Draw object
+	context->PSSetShaderResources(0, 1, &_debugTex.texture);
+	context->IASetVertexBuffers(0, 1, _debugGeo.vertexBuffer.GetAddressOf(), &_debugGeo.vertexBufferStride, &_debugGeo.vertexBufferOffset);
+	context->IASetIndexBuffer(_debugGeo.indexBuffer.Get(), DXGI_FORMAT_R16_UINT, 0);
+	context->DrawIndexed(_debugGeo.numOfIndices, 0, 0);
+#endif
 }
